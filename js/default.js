fd63db7d1850909cdd34dc1bd6ddebe0749da309
Mnyoki <mosesmunyoki6@gmail.com>
2023-01-30 14:11:57 +0300

    hosting hazzard.

--- js/default.js
+++ js/default.js
@@ -11,7 +11,7 @@ function getData(dataSource, divID)
   var obj = document.getElementById(divID);
 
   xhr.open("POST", dataSource);
-  xhr.setRequestHeader("Content-type", "application/x-www-form-encoded");
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
   xhr.onreadystatechange = function () 
   {
     if (xhr.readyState == 4 && xhr.status == 200) {
@@ -20,3 +20,28 @@ function getData(dataSource, divID)
   }
   xhr.send(null);
 }
+
+function fetchData(sourceData, id)
+{
+  xhr = false;
+
+  if (XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (ActiveXObject) {
+    xhr = new ActiveXObject("Microsoft.XMLHTTP")
+  }
+
+  var obj = document.getElementById(id);
+  var lname = document.getElementById("name").value;
+  var param = "lname=" + lname;
+
+  xhr.open("post", sourceData, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function ()
+  {
+    if (xhr.readyState == 4 & xhr.status == 200) {
+      obj.innerHTML = xhr.responseText;
+    }
+  }
+  xhr.send(param);
+} 
---
+++ temp/app.php
@@ -0,0 +1,10 @@
+<?php
+
+if (isset($_POST['lname'])) {
+  $lname = $_POST['lname'];
+  echo "<h1>Before they use me ama use my sense.</h1><p>Helo $lname. Its Amaizing you back.</p>";
+}
+
+echo "stay strong stay balling";
+
+?>
